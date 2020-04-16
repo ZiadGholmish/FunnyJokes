b21e6247072e1f7d5e28f9f4f2ef4b0920719880
Ziad Gholmish <ziad.gholmish@gmail.com>
2020-04-16 13:45:42 +0200

    Add the action to the backbutton

--- category-jokes/src/main/java/com/ziad/category_jokes/CategoryJokesActivity.kt
+++ category-jokes/src/main/java/com/ziad/category_jokes/CategoryJokesActivity.kt
@@ -1,15 +1,19 @@
 package com.ziad.category_jokes
 
 import android.os.Bundle
+import android.view.MenuItem
 import com.example.app_theme.BaseActivity
 import com.ziad.actions.actions.Actions
 import com.ziad.all_jokes.presentation.view.JokesFragment
+import kotlinx.android.synthetic.main.activity_category_jokes.*
+
 
 class CategoryJokesActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_category_jokes)
+        changeStatusBarIconsColor(toolbar)
         supportFragmentManager.beginTransaction().apply {
             add(
                 R.id.frameLayout,
@@ -24,6 +28,19 @@ class CategoryJokesActivity : BaseActivity() {
     }
 
     private fun setTitle() {
-        title = intent.getStringExtra(Actions.CATEGORY_NAME)
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayShowHomeEnabled(true)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_arrow_back_black_24dp);
+        tvTitle.text = intent.getStringExtra(Actions.CATEGORY_NAME)
+
     }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == android.R.id.home) {
+            finish()
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
 }
